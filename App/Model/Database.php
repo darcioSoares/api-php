4f74015f81ced070cd3647845414438a093fb4d2
darcioSoares <darciosoares28@gmail.com>
2020-09-24 15:44:12 -0300

    implementando a classe Database, estrutura inicial

--- App/Model/Database.php
+++ App/Model/Database.php
@@ -2,8 +2,43 @@
 
 namespace App\Model;
 
-class Database {
+use PDOException;
+use PDO;
+
+class Database {   
+   
+   private $dbname;
+   private $host;
+   private $user;
+   private $password;
+
+   private $conn;
+
+    public function __construct(){
+        $envPath = dirname(__FILE__) . "/../../env.ini";
+        $env = parse_ini_file($envPath);
+       
+        $this->dbname = $env["database"];
+        $this->host = $env["host"];
+        $this->user = $env["username"];
+        $this->password = $env["password"];
+        
+        try{
+            
+            $this->conn = new PDO("mysql:dbname=$this->dbname;host=$this->host",
+            $this->user, $this->password);
+
+            return $this->conn;
+        }catch(PDOException $e){
+            die("Error PDO conexao :" . $e->getMessage());
+        }        
+   }  
 
     
-    
 }
+
+// $envPath = dirname(__FILE__) . '/../env.ini';
+// $env = parse_ini_file($envPath);
+
+// $conn = new mysqli($env["host"],$env["username"], 
+// $env["password"], $env["database"]);
