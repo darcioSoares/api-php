a5d3bd670ea74cdcf4441a9a2901ab915b1d8b85
darcioSoares <darciosoares28@gmail.com>
2020-09-24 22:32:43 -0300

    conectando  arq Database e router com ControllerUser

--- App/Model/Database.php
+++ App/Model/Database.php
@@ -32,13 +32,73 @@ class Database {
         }catch(PDOException $e){
             die("Error PDO conexao :" . $e->getMessage());
         }        
-   }  
+   }
+
+
+   public function select(){
+
+       $stmt = $this->conn->prepare("SELECT * FROM users");
+
+       $stmt->execute();
+
+       $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+       return $result;
+
+   }
+
+   public function selectOne($id){
+       $stmt = $this->conn->prepare("SELECT * FROM users WHERE id = :id");
+
+       $stmt->bindParam(":id",$id);
+       $stmt->execute();
+       $result = $stmt->fetch(PDO::FETCH_ASSOC);
+
+       //return boolean
+       return $result;
+
+   }
+
+   public function create(array $param){
+
+       $stmt = $this->conn->prepare("INSERT INTO users(nome, sobrenome)
+               VALUES(:nome, :sobrenome)");
+        
+        $stmt->bindParam(":nome",$param["nome"]);
+        $stmt->bindParam(":sobrenome",$param["sobrenome"]);
+
+        $result = $stmt->execute();
+
+        // retorna um boolean
+        return $result;    
+
+   }
+
+   public function delete($id){
+
+        $stmt = $this->conn->prepare("DELETE FROM users WHERE id = :id");
+
+        $stmt->bindParam(":id",$id);
+
+        // retorna um boolean     
+        
+        $result = $stmt->execute();
+     
+
+     return $result;
+        
+
+   }
+
+//    public function update($id){
+       
+//        $stmt = $this->conn->prepare("UPDATE users SET nome = :nome,
+//                 sobrenome = :sobrenome WHERE id = :id");
+
+
+    
+//    }
 
     
 }
 
-// $envPath = dirname(__FILE__) . '/../env.ini';
-// $env = parse_ini_file($envPath);
-
-// $conn = new mysqli($env["host"],$env["username"], 
-// $env["password"], $env["database"]);
--- router/router.php
+++ router/router.php
@@ -2,6 +2,8 @@
 
 use App\Model\User;
 use Slim\App;
+use App\Model\Database;
+use App\Controller\ControllerUser;
 
 require_once('vendor/autoload.php');
 
@@ -13,13 +15,82 @@ $app = new App($config);
 
 
 //router
-$app->get('/', function ($request, $response, $args) {
-    return $response->write("my darcio ");
+$app->get('/', function($request, $response, $args) {
+
+    $data = ["nome"=>"joao"];
+
+    return $response->withJson($data,201);
 
 });
 
+//
+$app->post('/create', function($request, $response, $args){
+
+    $body = $request->getParsedBody();
+
+    $data = ControllerUser::ControllerCreate($body);
+
+     
+    if(!$data["error"]){
+        return $response->withJson($data,201);
+    }else{
+        return $response->withJson($data,400);
+    }
+
+});
+
+//
+$app->delete('/delete/{id}', function($request,$response, $args){
+
+    $id = $args["id"];
+
+    if(is_numeric($id)){
+
+        $data = ControllerUser::ControllerDelete($id);
+        if(!$data["error"]){
+            return $response->withJson($data, 201);
+        }else{
+            return $response->withJson($data, 401);
+        }
+
+    }else{
+        return $response->withJson(["id"=>"não numerico"]);
+    }
 
 
+});
+
+//
+$app->get('/select', function($request, $response, $args){
+
+    $data = ControllerUser::ControllerSelect();
+      
+    return $response->withJson($data,201);
+        
+});
+
+//
+$app->get('/selectone/{id}', function($request, $response, $args){
+
+    $id = $args["id"];
+
+    if(is_numeric($id)){
+
+        $data = ControllerUser::ControllerSelectOne($id);
+        
+        return $response->withJson($data, 201);
+       
+    }else{
+        return $response->withJson(["id"=>"não numerico"]);
+    } 
+
+});
+
+//
+// $app->put('/update/{id}', function($response, $request, $args){
+
+
+// });
 
 // Run app
 $app->run();
